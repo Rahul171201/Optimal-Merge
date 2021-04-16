a1a2aac48e45de40487c884f209c56144ba0e26e
Rahul Roy <70642284+Rahul171201@users.noreply.github.com>
2021-04-16 10:04:18 +0530

    Added solution to Optimal Merge pattern in cpp

---
+++ solution.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    
+    srand(time(NULL));
+    ll n;
+    cin>>n;
+    ll a[n];
+    for (int i = 0; i < n; ++i)
+    {
+        a[i]=rand()%100;
+    }
+    cout<<"Randomized Array:\n";
+    for (int i = 0; i < n; ++i)
+    {
+        cout<<a[i]<<" ";
+    }
+
+    priority_queue<ll> q;
+    for (int i = 0; i < n; ++i)
+    {
+        q.push(-1*a[i]);
+    }
+    ll ans=0;
+    while(!q.empty())
+    {   
+        ll x=-1*q.top();
+        q.pop();
+        if(q.empty())
+            break;
+        ll y=-1*q.top();
+        q.pop();
+ 
+        ans+=x+y;
+        q.push(  -1*(x+y) );
+
+    }
+    cout<<"\nTotal computation time:"<<ans;
+
+    return 0;
+}
